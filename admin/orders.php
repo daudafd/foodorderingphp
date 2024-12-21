c5bfa432cd98ac5001e3723da9b97b50763df561
Dauda Femi <daudafd@gmail.com>
2024-12-21 08:10:28 -0800

    datatable

--- admin/orders.php
+++ admin/orders.php
@@ -1,66 +1,66 @@
-	<!-- !PAGE CONTENT! -->
-	<div class="w3-main" style="margin-left:300px;margin-top:43px;">
+<div class="w3-main" style="margin-left:300px;margin-top:43px;">
 
-<!-- Header -->
-<header class="w3-container" style="padding-top:22px">
-  <h5><b><i class="fa fa-dashboard"></i> Orders</b></h5>
-</header>
+    <header class="w3-container" style="padding-top:22px">
+        <h5><b><i class="fa fa-dashboard"></i> Orders</b></h5>
+    </header>
 
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table id="order-table" class="table table-bordered table-striped"> <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Name</th>
+                                <th>Address</th>
+                                <th>Mobile</th>
+                                <th>Time</th>
+                                <th>Status</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $i = 1;
+                            include 'db_connect.php';
+                            $qry = $conn->query("SELECT * FROM orders ORDER BY created_at DESC"); // Order by time (newest first)
+                            while ($row = $qry->fetch_assoc()) :
+                            ?>
+                                <tr>
+                                    <td><?php echo $i++ ?></td>
+                                    <td><?php echo $row['name'] ?></td>
+                                    <td><?php echo $row['address'] ?></td>
+                                    <td><?php echo $row['mobile'] ?></td>
+                                    <td><?php echo $row['created_at'] ?></td>
+                                    <?php if ($row['status'] == 1) : ?>
+                                        <td class="text-center"><span class="badge bg-success">Confirmed</span></td>
+                                    <?php else : ?>
+                                        <td class="text-center"><span class="badge bg-secondary">For Verification</span></td>
+                                    <?php endif; ?>
+                                    <td>
+                                        <button class="btn btn-sm btn-primary view_order" data-id="<?php echo $row['id'] ?>">View Order</button>
+                                    </td>
+                                </tr>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <link href="https://cdn.datatables.net/v/bs5/jq-3.7.0/dt-2.1.8/af-2.7.0/b-3.2.0/b-colvis-3.2.0/date-1.5.4/r-3.0.3/sc-2.4.3/sb-1.8.1/datatables.min.css" rel="stylesheet">
+    <script src="https://cdn.datatables.net/v/bs5/jq-3.7.0/dt-2.1.8/af-2.7.0/b-3.2.0/b-colvis-3.2.0/date-1.5.4/r-3.0.3/sc-2.4.3/sb-1.8.1/datatables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#order-table').DataTable({ // Initialize DataTable on the table
+                "order": [[ 4, "desc" ]] // Default sort by "Time" column (index 4) descending
+            });
 
-<div class="container-fluid">
-<div class="card">
-		<div class="card-body">
-  <div class="table-responsive">
-    <table class="table table-bordered">
-      <thead>
-        <tr>
-        <th>#</th>
-			<th>Name</th>
-			<th>Address</th>
-			<th>Mobile</th>
-			<th>Time</th>
-			<th>Status</th>
-			<th></th>
-        </tr>
-      </thead>
-      <tbody>
-			<?php 
-			$i = 1;
-			include 'db_connect.php';
-			$qry = $conn->query("SELECT * FROM orders ");
-			while($row=$qry->fetch_assoc()):
-			 ?>
-			 <tr>
-			 		<td><?php echo $i++ ?></td>
-			 		<td><?php echo $row['name'] ?></td>
-			 		<td><?php echo $row['address'] ?></td>
-			 		<td><?php echo $row['mobile'] ?></td>
-					 <td><?php echo $row['created_at'] ?></td>
-			 		<?php if($row['status'] == 1): ?>
-			 			<td class="text-center"><span class="badge bg-success">Confirmed</span></td>
-			 		<?php else: ?>
-			 			<td class="text-center"><span class="badge bg-secondary">For Verification</span></td>
-			 		<?php endif; ?>
-			 		<td>
-           <!-- <button id="confirm_order_button" class="btn btn-success">Confirm Order</button> -->
-
-			 			<button id="confirm_order_button" class="btn btn-sm btn-primary view_order" data-id="<?php echo $row['id'] ?>" >View Order</button>
-			 		</td>
-			 </tr>
-			<?php endwhile; ?>
-		</tbody>
-    </table>
-  </div>
-</div>
-
-
-
-<script>
-    $(document).ready(function() {
-        $('.view_order').click(function() {
-            console.log("Button clicked");
-            uni_modal('Order','view_order.php?id=' + $(this).attr('data-id'));
+            $('.view_order').click(function() {
+                uni_modal('Order', 'view_order.php?id=' + $(this).attr('data-id'));
+            });
         });
-    });
-</script>
+    </script>
+
+</div>
